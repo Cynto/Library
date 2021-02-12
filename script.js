c0a90ceab6d96226831f988287eeaf7c78063e96
Cynto <lucagaravello@outlook.com>
2021-02-12 16:46:45 +0100

    fixed bookTotal error. Changed UI a bit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const bookCountText = document.querySelector('#bookCount');
 const bookCompletedText = document.querySelector('#bookCompletedCount');
 const bookTitleContainer = document.querySelector('#book-titles');
-const bookCardContainer = document.querySelector('#main-container')
+const bookCardContainer = document.querySelector('#main-container');
+const addContainer = document.querySelector('#add-container');
+
+const formSection = document.querySelector('.add-book-section');
+const form = document.querySelector('.new-book-form');
+
+
+
+
+
 //array which stores book objects
 let myLibrary = [];
 
@@ -15,14 +24,20 @@ function Book(title, author, pages, readIt) {
 }
 
 function addBookToLibrary(title, author, pages, readIt) {
-    
     const bookObject =  new Book(title, author, pages, readIt);
     myLibrary.push(bookObject);
+    console.log(myLibrary.length)
+    
 }
 addBookToLibrary('Harry Potter', 'JK Rowling', 500, true);
-addBookToLibrary('Harry Potter 2', 'JK Rowling', 500, false);
+
 
 function loopThroughArray() {
+    //if there is no title, the book object is removed from array
+    myLibrary = myLibrary.filter(a => a.title !== '')
+    //containers are reset so duplicates aren't made in the loop
+    bookTitleContainer.querySelectorAll('*').forEach(n => n.remove())
+    bookCardContainer.querySelectorAll('*').forEach(n => n.remove())
     bookCountText.textContent = 'Books: ' + myLibrary.length;
     //completedBooks contains all the books which have been read.
     let completedBooks = myLibrary.filter(a => a.readIt === true);
@@ -30,9 +45,12 @@ function loopThroughArray() {
     let bookTitleArray = myLibrary.map(a => a.title);
     let bookAuthorArray = myLibrary.map(a => a.author);
     let bookPagesArray = myLibrary.map(a => a.pages);
+   
     
     for(i = 0; i < myLibrary.length; i++) {
+          
         myLibrary[i].dataBook = i;
+        console.log('length: ' + myLibrary.length)
         console.log(myLibrary)
         const bookTitle = document.createElement('h4');
         bookTitle.textContent = bookTitleArray[i]
@@ -42,12 +60,12 @@ function loopThroughArray() {
         const bookCard = document.createElement('div');
         bookCard.classList.add('card');
         bookCardContainer.appendChild(bookCard);
-        
+
         bookCard.setAttribute('data-book', `${i}`)
 
         const bookCardTitle = document.createElement('div');
         bookCardTitle.classList.add('card-title');
-        bookCardTitle.textContent = bookTitleArray[i]
+        bookCardTitle.textContent ='Title: ' + bookTitleArray[i]
         bookCard.appendChild(bookCardTitle)
 
         const bookCardBottom = document.createElement('div');
@@ -56,7 +74,7 @@ function loopThroughArray() {
 
         const cardBottomLeft = document.createElement('div');
         cardBottomLeft.classList.add('card-bottom-left');
-        cardBottomLeft.textContent = bookAuthorArray[i]
+        cardBottomLeft.textContent = 'Author: ' + bookAuthorArray[i]
         bookCardBottom.appendChild(cardBottomLeft)
 
         const cardBottomRight = document.createElement('div');
@@ -77,6 +95,8 @@ function loopThroughArray() {
         notReadButton.classList.add('not-read-button');
         notReadButton.textContent = 'Not Read';
         notReadButton.setAttribute('data-book', `${i}`)
+
+        
         
         if(myLibrary[i].readIt === true) {
             buttonContainer.appendChild(readButton)
@@ -85,13 +105,18 @@ function loopThroughArray() {
             buttonContainer.appendChild(notReadButton)
         }
 
+        
+
         readButton.addEventListener('click', () => {
+            //when button is clicked, if statement checks whether the text content of the button is 'Read' or 'Not read'
             if(readButton.textContent === 'Read') {
                 readButton.classList.remove('read-button')
                 readButton.classList.add('not-read-button');
+                //If the text content is 'read', when clicked the button's class will be replaced with 'not-read-button' and the text content will be changed to not read.
                 readButton.textContent = 'Not Read';
                 for(i = 0; i < myLibrary.length; i++) {
                     if(myLibrary[i].readIt === true) {
+                        //if the book object is has a property of readIt set to true, when the button is clicked it'll be set to false and the completed books array is updated.
                         myLibrary[readButton.getAttribute('data-book')].readIt = false;
                         let completedBooks = myLibrary.filter(a => a.readIt === true);
                         bookCompletedText.textContent = 'Completed Books: ' + completedBooks.length;
@@ -147,6 +172,13 @@ function loopThroughArray() {
                 }
             }
         })
+
+        const cardDeleteButton = document.createElement('button');
+        cardDeleteButton.classList.add('card-delete');
+        cardDeleteButton.setAttribute('data-book', `${i}`)
+        bookCardTitle.appendChild(cardDeleteButton)
+
+        cardDeleteButton.addEventListener('click', deleteBooks);
        
         
         
@@ -155,7 +187,8 @@ function loopThroughArray() {
         deleteButton.value = `${deleteButton.getAttribute('data-book')}`
         console.log()
         //when delete button is clicked, it removes the book from all relevent fields.
-        deleteButton.addEventListener('click', () => {
+        deleteButton.addEventListener('click', deleteBooks)
+        function deleteBooks() {
             let click = deleteButton.value;
             click = Number(click)
             //if 
@@ -173,10 +206,11 @@ function loopThroughArray() {
                 oneBook.remove();
            }
 
-        })
+        }
         deleteButton.classList.add('delete-button');
         bookTitle.appendChild(deleteButton)
-
+       
+        
 
     }
     for(i = 0; i < myLibrary.length; i++) {
@@ -185,4 +219,56 @@ function loopThroughArray() {
     }
 }
 loopThroughArray()
+const submitButton = document.createElement('button');
+submitButton.classList.add('submit-button')
+form.appendChild(submitButton)
 
+const addButton = document.createElement('button');
+addButton.classList.add('add-button');
+
+addContainer.appendChild(addButton)
+
+addButton.addEventListener('click', () => {
+    formSection.setAttribute('style', 'visibility: visible');
+
+   
+
+    submitButton.addEventListener('click', () => {
+        //when submit button is clicked, values of fields are put in variables which are then put in addBookToLibrary function
+        let bookTitle = document.getElementById('book-title').value;
+        let bookAuthor = document.getElementById('book-author').value;
+        let bookPages = document.getElementById('book-pages').value;
+        bookPages = Number(bookPages)
+        let bookRead = '';
+        //if statement checks which radio button is checked.
+        if(document.getElementById('true').checked) {
+            bookRead = true;
+        }
+        else if(document.getElementById('false').checked) {
+            bookRead = false;
+        }
+        
+        formSection.setAttribute('style', 'visibility: hidden');
+        addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead)
+        //values are then reset.
+        document.getElementById('book-title').value = ''
+        document.getElementById('book-author').value = ''
+        document.getElementById('book-pages').value = ''
+        
+        loopThroughArray()
+
+    })
+    
+})
+
+const closeFormButton = document.querySelector('.close-form');
+
+closeFormButton.addEventListener('click', () => {formSection.setAttribute('style', 'visibility: hidden')});
+formSection.addEventListener('click', closeForm)
+
+function closeForm(e) {
+    console.log(e.target.toString())
+    if(e.target.toString() === '[object HTMLElement]') {
+        formSection.setAttribute('style', 'visibility: hidden');
+    }
+}
